f260d6f01e2aca11d4ba9ee257e166ff8a6441c0
William Mead <wmmead@ucdavis.edu>
2021-12-16 11:52:03 -0800

    third try

--- des157b/script.js
+++ des157b/script.js
@@ -38,3 +38,10 @@
         }, 1000);
     }
 })();
+
+const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+}
+window.addEventListener('resize', appHeight);
+appHeight();
